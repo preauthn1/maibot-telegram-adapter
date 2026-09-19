66a3035583c7452dc524b8c1e823cd8001716c48
root <root@VM-819bea6e-be88-440e-8c6f-e337544a9ed8.cs1local>
2026-09-19 14:19:44 +0800

    chore: 删除 pytests 测试目录

--- pytests/A_memorix_test/test_a_memorix_paths.py
+++
@@ -1,5 +0,0 @@
-from src.A_memorix.paths import default_data_dir, repo_root
-
-
-def test_default_data_dir_matches_runtime_template() -> None:
-    assert default_data_dir() == repo_root() / "data" / "a-memorix"
